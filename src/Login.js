55b941c3ec54620a27474f433ae74808bd1b833c
CarlosQuesadaSoto <48097502+CarlosQuesadaSoto@users.noreply.github.com>
2021-05-24 17:31:38 +0200

    prueba

--- src/Login.js
+++ src/Login.js
@@ -1,98 +1,65 @@
-import React from "react";
-import { BsFillPersonFill } from "react-icons/bs";
-import { FaUserAlt ,FaUnlock} from "react-icons/fa";
+ import React from 'react';
+ import "./Login.css";
 
-const Login = (props) => {
-  const {
-    email,
-    setEmail,
-    password,
-    setPassword,
-    handleLogin,
-    handleRegister,
-    handleLogout,
-    hasAccount,
-    setHasAccount,
-    emailError,
-    passwordError,
-  } = props;
+ const Login = (props) =>{
 
-  return (
-    <section className="login">
+    const{
+        email,
+        setEmail,
+        displayname,
+        setDisplayName,
+        password,
+        setPassword,
+        handleLogin,
+        handleRegister,
+        handleLogout,
+        hasAccount,
+        setHasAccount,
+        emailError,
+        passwordError
+    }=props;
 
+     return(
+         <section className="login">
+             <div className="loginContainer">
+                <img alt=""></img> 
+             {hasAccount ? (
+                         <>
+                            <label>Usuario</label>
+                 <input type="text" autoFocus required value={displayname} onChange={(e)=>setDisplayName(e.target.value)}/>
+                 <p className="errorMsg">{emailError}</p>
+
+                            <label>Email</label>
+                 <input type="text" autoFocus required value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                 <p className="errorMsg">{emailError}</p>
+
+                 <label>Contraseña</label>
+                 <input type="password" required value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                 <p className="errorMsg">{passwordError}</p>
+                 <div className="btnContainer">
+                            <button onClick={handleRegister}>Regístrate</button>
+                            <p>¿Tienes cuenta?<span onClick={()=> setHasAccount(!hasAccount)}>Inicia sesión</span></p>
+                            </div>
+                         </>
+                     ) : ( 
+                        <>
+                            <label>Usuario</label>
+                 <input type="text" autoFocus required value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                 <p className="errorMsg">{emailError}</p>
+
+                 <label>Contraseña</label>
+                 <input type="password" required value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                 <p className="errorMsg">{passwordError}</p>
+                 <div className="btnContainer">
+                            <button onClick={handleLogin}>Inicia sesión</button>
+                            <p>¿No tienes cuenta?<span onClick={()=> setHasAccount(!hasAccount)}>Regístrate</span></p>
+                            </div>
+                         </>
+                     )}
+                 
+             </div>
         
-      <div className="loginContainer-left">
-     
-
-          {hasAccount ? (
-            <>
-              <h1 className="text-white center mb-5">Registrate aqui!</h1>
-              <label>Email  <FaUserAlt width="40px" height="40px" /></label>
-              <input
-                type="text"
-                autoFocus
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <p className="errorMsg">{emailError}</p>
-
-              <label>Contraseña  <FaUnlock width="40px" height="40px" /></label>
-              <input
-                type="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <p className="errorMsg">{passwordError}</p>
-              <div className="btnContainer">
-                <button onClick={handleRegister}>Registrar</button>
-                <p>
-                  Ya tienes cuenta?{" "}
-                  <span onClick={() => setHasAccount(!hasAccount)}>
-                    Logeate
-                  </span>
-                </p>
-              </div>
-            </>
-          ) : (
-            <>
-            <h1 className="text-white center mb-5">Inicia Sesion!</h1>
-              <label>Email  <FaUserAlt width="40px" height="40px" /></label>
-              <input
-                type="text"
-                autoFocus
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <p className="errorMsg">{emailError}</p>
-
-              <label>Contraseña  <FaUnlock width="40px" height="40px" /></label>
-              <input
-                type="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <p className="errorMsg">{passwordError}</p>
-              <div className="btnContainer">
-                <button onClick={handleLogin}>Iniciar Sesion</button>
-                <p>
-                  No tienes cuenta ?{" "}
-                  <span onClick={() => setHasAccount(!hasAccount)}>
-                    Registrate
-                  </span>
-                </p>
-              </div>
-            </>
-          )}
-        </div>
-        
-      
-    </section>
-
-    
-  )
-};
-export default Login;
+         </section>
+     )
+ }
+ export default Login;
